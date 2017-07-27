d358d74ef61d7680bb92ebb69331f63b46683548
wangyang <419468714@qq.com>
2017-07-28 00:07:14 +0800

    播放暂停按钮完成。

--- song.js
+++ song.js
@@ -17,4 +17,18 @@ $(function(){
             $p.appendTo($lyric.children('.lines'))
         })
     })
+    let audio = document.createElement('audio')
+    audio.src = 'http://otpfh1rxr.bkt.clouddn.com/%E5%B2%81%E6%9C%88%E7%A5%9E%E5%81%B7-%E9%87%91%E7%8E%9F%E5%B2%90'
+    audio.oncanplay = function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    }
+    $('.pasue-play').on('click',function(){
+        audio.pause()
+        $('.disc-container').removeClass('playing')
+    })
+    $('.icon-play').on('click',function(){
+        audio.play()
+        $('.disc-container').addClass('playing')
+    })
 })
