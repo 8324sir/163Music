fe229c475405e1f4f102b50c3cb1fd4b77bbc22f
wangyang <419468714@qq.com>
2017-08-09 01:46:04 +0800

    热歌榜页面上方样式完成

--- index.js
+++ index.js
@@ -19,9 +19,30 @@ $(function(){
     })
     
     //监听事件
-    $('.home-nav').on('click','ol.tab-items>li',function(e){
-        let $li = $(e.currentTarget).addClass('active').siblings().removeClass('active')
+    $('.home-nav').on('click','ol.tab-items > li',function(e){
+        let $li = $(e.currentTarget).addClass('active')
+        $li.siblings().removeClass('active')
+        //console.log($li)
         let index = $li.index()
+        //console.log(index)
+        $li.trigger('tabChange',index)
         $('.tab-content > li').eq(index).addClass('active').siblings().removeClass('active')
+    })
+
+    $('.home-nav').on('tabChange',function(e,index){
+        let $li = $('.tab-content > li').eq(index)
+        if($li.attr('data-downloaded') === 'yes'){
+            return
+        }
+        if(index === 1){
+            $.get('./topsong.json').then(function(response){
+                $li.attr('data-downloaded','yes')
+            })
+        }
+        if(index === 2){
+            $.get('./search.json').then(function(response){
+                $li.attr('data-downloaded','yes')
+            })
+        }
     })
 })
